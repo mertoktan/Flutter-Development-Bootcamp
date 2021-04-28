b600a503f990c43bfefe425562f9e1b47e817e0f
Mert <mertoktanfb@gmail.com>
2021-04-28 23:57:10 +0300

    update commit

---
+++ dicee_flutter/lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text('Dicee'),
+        backgroundColor: Colors.red,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 5;
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button got pressed.');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
