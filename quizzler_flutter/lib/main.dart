291480504312e8da59706ab648cf976198502ed2
Mert <mertoktanfb@gmail.com>
2021-05-01 23:43:59 +0300

    score keeper update commit

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -27,6 +27,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -61,7 +72,15 @@ class _QuizPageState extends State<QuizPage> {
                 fontSize: 20.0,
               ),
             ),
-            onPressed: () {},
+            onPressed: () {
+              setState(() {
+                scoreKeeper.add(
+                    Icon(
+                  Icons.check,
+                  color: Colors.green,
+                ));
+              });
+            },
           ),
         )),
         Expanded(
@@ -79,7 +98,10 @@ class _QuizPageState extends State<QuizPage> {
             ),
             onPressed: () {},
           ),
-        ))
+        )),
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
