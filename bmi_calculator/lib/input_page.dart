fb11df36b1015a8f5774fbc101a3172a5cd8aa20
mertoktan <mertoktanfb@gmail.com>
2021-05-10 23:20:50 +0300

    Creating custom flutter widgets

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
 
-const bottomContainerHeight=80.0;
-const activeCardColour=Color(0xFF101E33);
-const bottomContainerColour=Color(0xFFEB1555);
-
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF101E33);
+const bottomContainerColour = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -25,11 +27,19 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     colour: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     colour: activeCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
+                    )
                   ),
                 ),
               ],
@@ -37,6 +47,9 @@ class _InputPageState extends State<InputPage> {
             Expanded(
               child: ReusableCard(
                 colour: activeCardColour,
+                cardChild: IconContent(
+                   icon: FontAwesomeIcons.,
+                )
               ),
             ),
             Expanded(
@@ -45,18 +58,20 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     colour: activeCardColour,
+                    cardChild:
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     colour: activeCardColour,
+                    cardChild:
                   ),
                 ),
               ],
             )),
             Container(
               color: bottomContainerColour,
-              margin: EdgeInsets.only(top:10.0),
+              margin: EdgeInsets.only(top: 10.0),
               width: double.infinity,
               height: bottomContainerHeight,
             )
@@ -65,19 +80,6 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour});
 
-  final Color colour;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
-  }
-}
+
